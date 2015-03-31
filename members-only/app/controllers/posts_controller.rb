c335c14212d56fa8647563c77c8e2da5c1f6681a
Andrew Chung <andrew.chung159@gmail.com>
2015-03-31 18:44:01 -0400

    Create post submission form

--- members-only/app/controllers/posts_controller.rb
+++ members-only/app/controllers/posts_controller.rb
@@ -1,11 +1,27 @@
 class PostsController < ApplicationController
 	before_action :signed_in_user, only: [:new, :create]
+	
 	def new
+		@post = Post.new
 	end
 
 	def create
+		@post = current_user.posts.create(post_params)
+		if @post.save
+			flash[:success] = 'Post created!'
+			redirect_to posts_path
+		else
+			flash.now[:error] = 'Post must be between 5 to 150 characters!'
+			render 'new'
+		end
 	end
 
 	def index
+		@posts = Post.all
 	end
+
+	private
+		def post_params
+			params.require(:post).permit(:body)
+		end
 end
--- members-only/app/models/post.rb
+++ members-only/app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
+
+	validates :body, presence: true, length: { in: 1..150 }
 end
