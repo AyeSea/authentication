485e14bd7ec37d212782dd6440136cd21ef8b07f
Andrew Chung <andrew.chung159@gmail.com>
2015-03-30 22:04:11 -0400

    Update so remember_token created/saved when new user created

--- members-only/app/models/user.rb
+++ members-only/app/models/user.rb
@@ -1,8 +1,24 @@
 class User < ActiveRecord::Base
+	before_create :save_token
+
+
 	validates :username, presence: true, uniqueness: true, length: { in: 5..20 }
 	validates :email, presence: true, uniqueness: { case_sensitive: false },
 										length: { in: 5..30 }
 
 	has_secure_password
 	validates :password, length: { in: 8..30 }
+
+	private
+		def save_token
+			self.remember_token = User.encrypt_token
+		end
+
+		def User.encrypt_token
+			Digest::SHA1.hexdigest(User.create_token)
+		end
+
+		def User.create_token
+			SecureRandom.urlsafe_base64
+		end
 end
