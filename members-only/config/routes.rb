7af8edbed50139511230374f544758f655760009
Andrew Chung <andrew.chung159@gmail.com>
2015-03-30 21:43:20 -0400

    Add Sessions controller and signin view

--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get    'signin'  => 'sessions#new'
+  delete 'signout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
