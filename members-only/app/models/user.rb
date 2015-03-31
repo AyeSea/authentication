73de8229d1607de4160b38f19feb19d9ff35466e
Andrew Chung <andrew.chung159@gmail.com>
2015-03-30 21:21:11 -0400

    Add validations (inc. has_secure_password) to User

--- members-only/app/models/user.rb
+++ members-only/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	validates :username, presence: true, uniqueness: true, length: { in: 5..20 }
+	validates :email, presence: true, uniqueness: { case_sensitive: false },
+										length: { in: 5..30 }
+										
+	has_secure_password
+	validates :password, length: { in: 8..30 }
 end
